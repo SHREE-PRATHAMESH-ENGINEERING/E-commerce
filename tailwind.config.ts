55f07900d15e504f9a2a1a752b0d5536ed650e8e
Eva Sabeeh <evas190401@gmail.com>
2025-07-27 18:32:46 +0530

    Project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-yellow':'#FED700',
+        'primary-color':'#051937',
+        'secondary-color': '#A8EB12',
+      }
+    },
+  },  
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("daisyui")],
+};
+export default config;
